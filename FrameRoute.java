9529e1f81e4ba6ba12939f57300e51f4c50cdff5
Kylian LucasThuillier <kylian.lucast@gmail.com>
2024-06-03 16:54:28 +0200

    Modification Route.java et FrameRoute.java

--- FrameRoute.java
+++ FrameRoute.java
@@ -4,19 +4,35 @@ import java.awt.*;
 
 public class FrameRoute extends JFrame
 {
+	/*--------------*/
+	/* Variables    */
+	/*--------------*/
+
 	private JLabel tronconsJLabel;
 	private JLabel departJLabel;
-	private JLabel arriveJLabel;
+	private JLabel arriverJLabel;
 
 	private JTextField tronconsJTextField;
 
 	private JComboBox departJComboBox;
-	private JComboBox arriveJComboBox;
+	private JComboBox arriverJComboBox;
 
 	private JButton validerJButton;
 
+	private int   nbTroncons;
+	private Ville villeDepart;
+	private Ville villeArriver;
+
+	/*private ArrayList<Ville> listeVille;*/
+
+
+	/*--------------*/
+	/* Instructions */
+	/*--------------*/
+
 	public FrameRoute()
 	{
+
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.setTitle ("Ajouter Routes");
 		this.setLocation(10, 10);
@@ -24,21 +40,27 @@ public class FrameRoute extends JFrame
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 
-
 		this.setVisible(true);
 
-		// Label
+
+
+		// Initialisation des Label
 		this.tronconsJLabel = new JLabel("Nombre de tronçons");
 		this.departJLabel   = new JLabel("Ville de départ"   );
-		this.arriveJLabel   = new JLabel("Ville d'arrivé"    );
+		this.arriverJLabel   = new JLabel("Ville d'arrivé"    );
 
-		this.departJComboBox = new JComboBox();
-		this.arriveJComboBox = new JComboBox();
+		// Initialisation des ComboBox
+		this.departJComboBox = new JComboBox(/*this.listeVille*/);
+		this.arriverJComboBox = new JComboBox(/*this.listeVille*/);
 
+		//Initialisation du TextField
 		this.tronconsJTextField = new JTextField(11);
 
+		//Initialisation du Boutton
 		this.validerJButton = new JButton("Valider");
 
+
+		// Ajout des Composants
 		Insets insets = new Insets(10, 10, 10, 5);
 
 		gbc.gridx = 0;
@@ -61,11 +83,11 @@ public class FrameRoute extends JFrame
 
 		gbc.gridx = 0;
 		gbc.gridy = 2;
-		this.add(arriveJLabel, gbc);
+		this.add(arriverJLabel, gbc);
 
 		gbc.gridx = 1;
 		gbc.gridy = 2;
-		this.add(arriveJComboBox, gbc);
+		this.add(arriverJComboBox, gbc);
 
 		gbc.gridx = 0;
 		gbc.gridy = 3;
@@ -75,6 +97,22 @@ public class FrameRoute extends JFrame
 		this.pack();
 		this.setVisible(true);
 	}
+
+/*
+	public void itemStateChanged(ItemEvent e) 
+	{
+		// si l'état d'un combobox est modifiée 
+		switch (e.getSource()) {
+			case deparComboBox:
+				this.villeDepart = villeDepart.getSelectedItem();
+			case arriverComboBox:
+				this.villeArriver = villeArriver.getSelectedItem();
+			default:
+				this.villeDepart = null;
+				break;
+			}
+	}
+*/
 
 	public static void main(String[] a)
 	{
--- Route.java
+++ Route.java
@@ -1,6 +1,6 @@
 public class Route {
     
-    private int nbTroncons;
+    private int   nbTroncons;
     private Ville villeDepart;
     private Ville villeArriver;
 
@@ -8,13 +8,13 @@ public class Route {
     {
         if((nbTroncons >= 0 && nbTroncons <= 10) && villeDepart != null && villeArriver != null)
         {
-            this.nbTroncons = nbTroncons;
-            this.villeDepart = villeDepart;
+            this.nbTroncons   = nbTroncons;
+            this.villeDepart  = villeDepart;
             this.villeArriver = villeArriver;
         }
     }
 
-    public int getNbTroncons() { return this.nbTroncons; }
-    public Ville getVilleDepart() { return this.villeDepart; }
+    public int getNbTroncons()     { return this.nbTroncons;   }
+    public Ville getVilleDepart()  { return this.villeDepart;  }
     public Ville getVilleArriver() { return this.villeArriver; }
 }
