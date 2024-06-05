be8f98fa187a855fcd667cd7c5ebaa26589b2813
Kylian LucasThuillier <kylian.lucast@gmail.com>
2024-06-05 15:44:39 +0200

    Modif Route.java

--- Route.java
+++ Route.java
@@ -1,20 +1,29 @@
 public class Route {
-    
-    private int   nbTroncons;
-    private Ville villeDepart;
-    private Ville villeArriver;
 
-    public Route(int nbTroncons, Ville villeDepart, Ville villeArriver)
-    {
-        if((nbTroncons >= 0 && nbTroncons <= 10) && villeDepart != null && villeArriver != null)
-        {
-            this.nbTroncons   = nbTroncons;
-            this.villeDepart  = villeDepart;
-            this.villeArriver = villeArriver;
-        }
-    }
+	private int   nbTroncons;
+	private Ville villeDepart;
+	private Ville villeArriver;
 
-    public int getNbTroncons()     { return this.nbTroncons;   }
-    public Ville getVilleDepart()  { return this.villeDepart;  }
-    public Ville getVilleArriver() { return this.villeArriver; }
+	// Fabrique de Route
+	public static Route creerRoute(int nbTroncons, Ville villeDepart, Ville villeArriver)
+	{
+		if(nbTroncons <= 0 || nbTroncons >= 10) { return null; }
+		else if(villeDepart  == null)           { return null; }
+		else if(villeArriver == null)           { return null; }
+		else { return new Route(nbTroncons, villeDepart, villeArriver); }
+	}
+
+
+	private Route(int nbTroncons, Ville villeDepart, Ville villeArriver)
+	{
+		this.nbTroncons   = nbTroncons;
+		this.villeDepart  = villeDepart;
+		this.villeArriver = villeArriver;
+
+	}
+
+	// Accesseurs
+	public int getNbTroncons()     { return this.nbTroncons;   }
+	public Ville getVilleDepart()  { return this.villeDepart;  }
+	public Ville getVilleArriver() { return this.villeArriver; }
 }
